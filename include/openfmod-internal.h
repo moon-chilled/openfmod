757e2decb946a048fc8df12472157071cbf4ef65
Maya Rashish <maya@NetBSD.org>
2021-01-24 10:34:59 +0200

    Update to latest gorilla-audio API

--- include/openfmod-internal.h
+++ include/openfmod-internal.h
@@ -31,7 +31,7 @@ typedef struct {
 
 struct FMOD_SOUND {
 	bool is_mem, is_loaded;
-	ga_Handle *handle;
+	GaHandle *handle;
 	const char *fpath;
 };
 
@@ -65,9 +65,9 @@ FMOD_GUID *path_guid_get(MapPathToGuid *map, const char *key);
 
 struct FMOD_STUDIO_SYSTEM {
 	struct {
-		gau_Manager *mgr;
-		ga_Mixer *mixer;
-		ga_StreamManager *stream_mgr;
+		GauManager *mgr;
+		GaMixer *mixer;
+		GaStreamManager *stream_mgr;
 	} ga;
 
 	MapPathToGuid pathmap;
--- src/fsb-file.c
+++ src/fsb-file.c
@@ -51,9 +51,9 @@ static bool ensure_sound_loaded(FMOD_STUDIO_SYSTEM *sys, FMOD_SOUND *snd) {
 	if (snd->is_loaded) return true;
 
 	if (snd->is_mem) {
-		snd->handle = gau_create_handle_buffered_file(sys->ga.mixer, sys->ga.stream_mgr, snd->fpath, GAU_AUDIO_TYPE_OGG, NULL, NULL, NULL);
+		snd->handle = gau_create_handle_buffered_file(sys->ga.mixer, sys->ga.stream_mgr, snd->fpath, GauAudioType_Ogg, NULL, NULL, NULL);
 	} else {
-		ga_Sound *sound = gau_load_sound_file(snd->fpath, GAU_AUDIO_TYPE_OGG);
+		GaSound *sound = gau_load_sound_file(snd->fpath, GauAudioType_Ogg);
 		if (!sound) return false;
 		snd->handle = gau_create_handle_sound(sys->ga.mixer, sound, NULL, NULL, NULL);
 	}
